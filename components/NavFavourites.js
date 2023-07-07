18ebae5842a386b10ff9e334b93089c0ccd7ce21
Arthur Kabera <arthurkabera@gmail.com>
2023-07-07 15:14:18 +0300

    Working version of NavFavourites.js

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,52 +1,119 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { FlatList } from "react-native-gesture-handler";
-import { TouchableOpacity } from "react-native";
-import { Icon } from "react-native-elements";
+import React, { useState, useMemo } from "react";
+import { View, Text, TouchableOpacity, Modal, TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import RadioGroup from "react-native-radio-buttons-group";
 import tw from "tailwind-react-native-classnames";
 
-const data = [
-  {
-    id: "123",
-    icon: "home",
-    location: "Home",
-    destination: "Code Street, London, UK",
-  },
-  {
-    id: "456",
-    icon: "briefcase",
-    location: "Work",
-    destination: "London Eye, London, UK",
-  },
-];
-
 const NavFavourites = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [promoCode, setPromoCode] = useState("");
+
+  const radioButtons = useMemo(
+    () => [
+      {
+        id: "cash",
+        label: "Cash\nPay when trip ends",
+        value: "cash",
+      },
+      {
+        id: "card",
+        label: "Card\nPay when trip ends",
+        value: "card",
+      },
+      {
+        id: "wallet",
+        label: "Wallet\nPay Instantly",
+        value: "wallet",
+      },
+    ],
+    []
+  );
+
+  const [selectedId, setSelectedId] = useState("cash");
+
+  const handleConfirm = () => {
+    console.log("Selected payment method:", selectedId);
+    console.log("Promo code entered:", promoCode ? true : false, promoCode);
+    setModalVisible(false);
+  };
+
   return (
-    <FlatList
-      data={data}
-      keyExtractor={(item) => item.id}
-      ItemSeparatorComponent={() => {
-        <View style={[tw`bg-gray-200`, { height: 0.5 }]} />;
-      }}
-      renderItem={({ item: { location, destination, icon } }) => (
-        <TouchableOpacity style={tw`flex-row items-center p-5`}>
-          <Icon
-            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
-            name={icon}
-            type="ionicon"
-            color="white"
-            size={18}
-          />
-          <View>
-            <Text style={tw`font-semibold text-lg`}>{location}</Text>
-            <Text style={tw`text-gray-500`}>{destination}</Text>
+    <View>
+      {/* Touchable Opacity */}
+      <View style={tw`py-2 px-6`}>
+        <TouchableOpacity
+          style={tw`border-gray-700 border rounded-sm p-4 bg-yellow-400`}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={tw`text-gray-700 text-xs uppercase`}>Paying Via</Text>
+          <View style={tw`flex-row justify-between`}>
+            <Ionicons name="cash" size={24} color="black" />
+            <View>
+              <Text style={tw`text-lg font-semibold`}>Payment Method</Text>
+              <Text style={tw`text-sm font-semibold`}>When to Pay</Text>
+            </View>
+            <Ionicons name="arrow-forward" size={24} color="black" />
           </View>
         </TouchableOpacity>
-      )}
-    />
+
+        <View style={tw`py-4`}>
+          <TouchableOpacity
+            style={tw`border-gray-700 border rounded-sm p-4 bg-gray-900 justify-center items-center`}
+          >
+            <Text
+              style={tw`text-white uppercase font-bold text-lg`}
+              onPress={() => console.log("Ride Now")}
+            >
+              Ride Now
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* Modal */}
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View style={tw`m-4 p-4 bg-white rounded-sm`}>
+          <View style={tw`flex-row justify-between`}>
+            <Text style={tw`text-lg text-gray-900`}>Select Payment Method</Text>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <Text style={tw`text-lg`}>X</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={tw`text-sm text-gray-900 font-semibold`}>
+            Choose your preferred payment method
+          </Text>
+
+          {/* Payment options */}
+          <RadioGroup
+            radioButtons={radioButtons}
+            onPress={setSelectedId}
+            selectedId={selectedId}
+          />
+
+          {/* Promo code entry */}
+
+          <View style={tw`pt-2`}>
+            <TextInput
+              style={tw`border rounded-sm bg-white p-2`}
+              placeholder="Enter Promo Code"
+              value={promoCode}
+              onChangeText={setPromoCode}
+            />
+          </View>
+
+          {/* Confirm button */}
+          <View style={tw`pt-2`}>
+            <TouchableOpacity
+              style={tw`bg-yellow-400 p-2 rounded-sm justify-center items-center`}
+              onPress={handleConfirm}
+            >
+              <Text style={tw`font-bold text-gray-900 text-lg`}>Confirm</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </View>
   );
 };
 
 export default NavFavourites;
-
-const styles = StyleSheet.create({});
