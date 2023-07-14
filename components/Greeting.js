048e0aa0cf080f5d3e1e5688a0ab40af42d1724d
Arthur Kabera <arthurkabera@gmail.com>
2023-07-14 14:36:53 +0300

    Updated Greetings.js component to render name dynamically

--- components/Greeting.js
+++ components/Greeting.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { useSelector } from "react-redux";
 
 const Greeting = () => {
+  const user = useSelector(selectUser);
+
+  // Limit the name to just the first name entered
+  const getFirstWord = (str) => str.split(" ")[0];
+  const userName = getFirstWord(user.name);
+
   const [greeting, setGreeting] = useState("");
 
   useEffect(() => {
@@ -11,11 +18,11 @@ const Greeting = () => {
       let greetingText = "";
 
       if (currentHour >= 5 && currentHour < 12) {
-        greetingText = "Good Morning";
+        greetingText = "Good Morning " + userName;
       } else if (currentHour >= 12 && currentHour < 18) {
-        greetingText = "Good Afternoon";
+        greetingText = "Good Afternoon " + userName;
       } else {
-        greetingText = "Good Evening";
+        greetingText = "Good Evening " + userName;
       }
 
       setGreeting(greetingText);
