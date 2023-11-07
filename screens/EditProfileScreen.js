885240f0f058e5c06ba87f3d57176a9308cdb308
Arthur Kabera <arthurkabera@gmail.com>
2023-11-07 03:24:29 +0300

    EditProfile needs refining but uploads working successfully

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -15,48 +15,102 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectPerson, setPerson } from "../slices/personSlice";
 import { db, auth } from "../firebaseConfig";
 import { ActivityIndicator } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
 
 const EditProfileScreen = () => {
-  // const person = useSelector(selectPerson);
   const navigation = useNavigation();
   const firstUser = useSelector((state) => state.user.user);
-  const [userName, setName] = useState(firstUser["name"]); // Initialize with existing name
-
+  const [userName, setName] = useState(firstUser["name"]);
   const [person, setPerson] = useState(null);
   const [userUID, setUserUID] = useState(null);
+  const [image, setImage] = useState(null);
 
-  // Get the Current User's UID
+  const [downloadURL, setDownloadURL] = useState(null);
+  const [imageError, setImageError] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [licenseFileName, setLicenseFileName] = useState("");
+
+  const handleImageSelect = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      console.log("Permission to access media library is required!");
+      return;
+    }
+
+    try {
+      const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+      if (!pickerResult.canceled) {
+        setImage(pickerResult.assets[0].uri);
+        console.log(pickerResult.assets[0].uri);
+
+        const imageUri = pickerResult.assets[0].uri;
+        const userUid = auth.currentUser.uid;
+        const timestamp = new Date().getTime();
+
+        // Extract file extension from the image's URI
+        const fileExtension = imageUri.split(".").pop();
+        const filename = `${userUid}-${timestamp}-lc.${fileExtension}`;
+
+        setPhoto(pickerResult.assets[0].uri);
+        setLicenseFileName(filename);
+        console.log("File Name: " + filename);
+
+        const response = await fetch(imageUri);
+        const blob = await response.blob();
+
+        const storageRef = firebase
+          .storage()
+          .ref()
+          .child(`documents/profile-pictures/${filename}`);
+
+        try {
+          await storageRef.put(blob);
+          console.log("Image uploaded successfully");
+
+          // Get the download URL of the uploaded file
+          const downloadURL = await storageRef.getDownloadURL();
+          console.log("Download URL:", downloadURL);
+
+          // Now you can use the downloadURL as needed, for example, store it in a state
+          setDownloadURL(downloadURL);
+        } catch (error) {
+          console.error("Error uploading image: ", error);
+          setImageError(error.message);
+        }
+      }
+    } catch (error) {
+      console.error("Error handling image: ", error);
+      // You should handle the error here, such as displaying an error message to the user.
+    }
+  };
+
   useEffect(() => {
-    // Define the cleanup function for unsubscribing
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        // User is signed in.
         const uid = user.uid;
         console.log("User UID:", uid);
         setUserUID(uid);
       } else {
-        // No user is signed in.
         console.log("No user is signed in.");
       }
     });
 
-    // Clean up the listener when the component unmounts
     return () => {
       unsubscribe();
     };
   }, []);
 
-  // Get User Details from Riders Collection
   useEffect(() => {
     if (userUID) {
-      // Define the query to get the rider document based on userUID
       const query = db.collection("riders").where("authID", "==", userUID);
-
-      // Subscribe to Firestore updates for the query
       const unsubscribe = query.onSnapshot((querySnapshot) => {
         if (!querySnapshot.empty) {
-          // Document matching the condition exists
-          const doc = querySnapshot.docs[0]; // Access the first (and only) document
+          const doc = querySnapshot.docs[0];
           const riderData = doc.data();
           riderData.documentId = doc.id;
           setPerson(riderData);
@@ -66,48 +120,74 @@ const EditProfileScreen = () => {
         }
       });
 
-      // Clean up the listener when the component unmounts
       return () => unsubscribe();
     }
   }, [userUID]);
 
-  // Inside your component
-
   const dispatch = useDispatch();
 
-  const updateProfile = () => {
+  const updateProfile = async (documentId) => {
     const updatedName = userName.trim();
 
     if (updatedName) {
       db.collection("riders")
-        .where("authID", "==", firstUser["authID"])
-        .get()
-        .then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            doc.ref
-              .update({
-                name: updatedName,
-              })
-              .then(() => {
-                console.log("Profile successfully updated!");
-                // Dispatch an action to update the person state in Redux store
-                /*
-                dispatch(
-                  setPerson({
-                    ...person, // take all existing fields from person
-                    name: updatedName, // override the name field
-                  })
-                );
-                */
-                navigation.goBack();
-              })
-              .catch((error) => {
-                console.error("Error updating document: ", error);
-              });
-          });
+        .doc(documentId)
+        .update({
+          name: updatedName,
+        })
+        .then(async () => {
+          console.log("Name successfully updated!");
+
+          if (image) {
+            const userUid = auth.currentUser.uid;
+            const timestamp = new Date().getTime();
+            const fileExtension = image.split(".").pop();
+            const filename = `${userUid}-${timestamp}-profile.${fileExtension}`;
+
+            const response = await fetch(image);
+            const blob = await response.blob();
+
+            console.log("Filename: ", filename);
+
+            const storageRef = firebase
+              .storage()
+              .ref()
+              .child(`documents/profile-pictures/${filename}`);
+
+            storageRef.put(blob);
+            console.log("Image uploaded successfully");
+
+            /*
+
+            try {
+              await storageRef.put(blob);
+              console.log("Image uploaded successfully");
+
+              const downloadURL = await storageRef.getDownloadURL();
+              console.log("Download URL:", downloadURL);
+
+              db.collection("riders")
+                .doc(documentId)
+                .update({
+                  profilePicture: downloadURL,
+                })
+                .then(() => {
+                  console.log("Profile picture updated successfully");
+                  navigation.goBack();
+                })
+                .catch((error) => {
+                  console.error("Error updating profile picture: ", error);
+                });
+            } catch (error) {
+              console.error("Error uploading image: ", error);
+            }
+            */
+          } else {
+            navigation.goBack();
+          }
         })
         .catch((error) => {
-          console.log("Error getting documents: ", error);
+          console.error("Error updating document: ", error);
         });
     }
   };
@@ -128,37 +208,67 @@ const EditProfileScreen = () => {
       <View>
         {person ? (
           <>
-            <View style={[tw`items-center mt-4`]}>
-              <Image
-                source={{ uri: person.profilePicture }}
-                style={[
-                  tw`w-24 h-24 mb-4`,
-                  {
-                    borderRadius: 100,
-                  },
-                ]}
-              />
-            </View>
+            <TouchableOpacity onPress={handleImageSelect}>
+              <View style={[tw`items-center mt-4`]}>
+                {image ? (
+                  <Image
+                    source={{ uri: image }}
+                    style={[
+                      tw`w-24 h-24 mb-4`,
+                      {
+                        borderRadius: 100,
+                      },
+                    ]}
+                  />
+                ) : (
+                  <Image
+                    source={{ uri: person.profilePicture }}
+                    style={[
+                      tw`w-24 h-24 mb-4`,
+                      {
+                        borderRadius: 100,
+                      },
+                    ]}
+                  />
+                )}
+              </View>
+            </TouchableOpacity>
           </>
         ) : (
           <View style={tw`text-center`}>
             <ActivityIndicator size="large" color="#030813" />
           </View>
         )}
-        <Text style={tw`text-lg text-gray-600 mt-4`}>Name</Text>
+        {person ? (
+          <Text style={tw`text-lg text-gray-600 mt-4`}>
+            Name: {person.documentId}
+          </Text>
+        ) : null}
         <TextInput
           style={tw`border border-gray-300 mt-2 p-2 rounded-sm`}
           value={userName}
           onChangeText={setName}
         />
       </View>
-      <TouchableOpacity
-        style={[tw`mt-5 py-3 px-6 rounded-sm items-center`, styles.customColor]}
-        onPress={updateProfile}
-        disabled={!userName}
-      >
-        <Text style={tw`text-gray-900 font-bold text-lg`}>Update Profile</Text>
-      </TouchableOpacity>
+
+      {person ? (
+        <TouchableOpacity
+          style={[
+            tw`mt-5 py-3 px-6 rounded-sm items-center`,
+            styles.customColor,
+          ]}
+          onPress={() => updateProfile(person.documentId)}
+          disabled={!userName || !person.documentId}
+        >
+          <Text style={tw`text-gray-900 font-bold text-lg`}>
+            Update Profile
+          </Text>
+        </TouchableOpacity>
+      ) : (
+        <View style={tw`text-center`}>
+          <ActivityIndicator size="large" color="#030813" />
+        </View>
+      )}
     </SafeAreaView>
   );
 };
